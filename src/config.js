e9ddef2196e45404b4773fcdbe0fac8ccbf8912f
Luis Cadillo <lusicaf3r@Gmail.com>
2022-04-02 23:55:14 -0500

    Add main services and first iteration of AuthProvider

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const BASE_URI = "https://api-organizable.herokuapp.com/";
+export const tokenKey = "organizable_token";
+export const appKey = "organizable";
---
+++ src/context/auth-context.jsx
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from 'react';
+
+const AuthContext = React.createContext();
+
+const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => {
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => {
+  return context = useContext(AuthContext);
+};
+
+export { AuthProvider, useAuth };

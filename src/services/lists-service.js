6988e3d8b1faa40984434caf39386eec98284c98
Luis Cadillo <lusicaf3r@Gmail.com>
2022-04-05 22:21:29 -0500

    Add cards service

--- src/services/lists-service.js
+++ src/services/lists-service.js
@@ -1,8 +1,20 @@
 import apiFetch from "./api-fetch";
 
-export const createList = (list) => {
-  return apiFetch(`boards/${}/lists`, {
-    method: "POST",
+export const createList = (list, boardId) => {
+  return apiFetch(`boards/${boardId}/lists`, {
     body: JSON.stringify(list),
+  });
+};
+
+export const updateList = (list, boardId) => {
+  return apiFetch(`boards/${boardId}/lists/${list.id}`, {
+    method: "PATCH",
+    body: JSON.stringify({name: list.name}),
+  });
+};
+
+export const destroyList = (list, boardId) => {
+  return apiFetch(`boards/${boardId}/lists/${list.id}`, {
+    method: "DELETE",
   });
 };
